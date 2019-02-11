977dc878dba1fe25d9a1c74c3d1e2b4b3b3ea4d0
inspectorr <44624950+inspectorr@users.noreply.github.com>
2019-02-12 02:23:50 +0300

    portfolio first commit

---
+++ simpledraw/precache-manifest.0cd9688f218224c6447a2a1f4df54724.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e41b86c65cc32576ac8",
+    "url": "/static/js/main.0e41b86c.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "0e41b86c65cc32576ac8",
+    "url": "/static/css/main.37dba701.chunk.css"
+  },
+  {
+    "revision": "722f03f5f6cb3af36ab5699d73adc82e",
+    "url": "/index.html"
+  }
+];
