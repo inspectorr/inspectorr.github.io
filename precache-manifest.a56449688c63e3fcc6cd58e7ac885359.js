a9c5689a493dc06d0e4e966d4e8dec0df7b6dfa6
inspectorr <44624950+inspectorr@users.noreply.github.com>
2019-02-19 00:30:10 +0300

    portfolio update

---
+++ precache-manifest.a56449688c63e3fcc6cd58e7ac885359.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "47b75ae3a231d5c3dee713f6af826784",
+    "url": "/static/media/spaceship.47b75ae3.JPG"
+  },
+  {
+    "revision": "f2446d23f37b88c1afde30e4acccd527",
+    "url": "/static/media/simpledraw.f2446d23.JPG"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5a36776f1ee0a204bb6e",
+    "url": "/static/js/main.5a36776f.chunk.js"
+  },
+  {
+    "revision": "12a92711b1afe2b050cc",
+    "url": "/static/js/2.12a92711.chunk.js"
+  },
+  {
+    "revision": "5a36776f1ee0a204bb6e",
+    "url": "/static/css/main.47d5a8cc.chunk.css"
+  },
+  {
+    "revision": "12a92711b1afe2b050cc",
+    "url": "/static/css/2.1b48d26f.chunk.css"
+  },
+  {
+    "revision": "7f3e03fa9d482db20c4b1a3afc0e2d62",
+    "url": "/index.html"
+  }
+];
