e6730e8740b117bb9859871abcb242a7b1e7ce98
inspectorr <hmrfins@gmail.com>
2019-03-26 00:50:29 +0300

    update

---
+++ precache-manifest.6353526e6bcfc34c9ab0dfabd8ade218.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "47b75ae3a231d5c3dee713f6af826784",
+    "url": "./static/media/spaceship.47b75ae3.JPG"
+  },
+  {
+    "revision": "f2446d23f37b88c1afde30e4acccd527",
+    "url": "./static/media/simpledraw.f2446d23.JPG"
+  },
+  {
+    "revision": "6b6314de17933db7e261a07f192bf790",
+    "url": "./static/media/charts.6b6314de.JPG"
+  },
+  {
+    "revision": "610edaf39e436e4332ac4ba515646385",
+    "url": "./static/media/car-configurator.610edaf3.JPG"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "9043f7ee253c3220c6de",
+    "url": "./static/js/main.9043f7ee.chunk.js"
+  },
+  {
+    "revision": "2287e606d1dca9d514cf",
+    "url": "./static/js/2.2287e606.chunk.js"
+  },
+  {
+    "revision": "9043f7ee253c3220c6de",
+    "url": "./static/css/main.18113d11.chunk.css"
+  },
+  {
+    "revision": "2287e606d1dca9d514cf",
+    "url": "./static/css/2.1b48d26f.chunk.css"
+  },
+  {
+    "revision": "e82d0e084d510d46b75f64fc5a65d132",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.16e810f13909f49ec52fccba4c980cea.js"
+  "./precache-manifest.6353526e6bcfc34c9ab0dfabd8ade218.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
