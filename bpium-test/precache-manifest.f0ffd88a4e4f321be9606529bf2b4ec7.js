f208e3d813440e7c5a08d650673a093a4af139f2
inspectorr <hmrfins@gmail.com>
2019-04-15 00:25:59 +0300

    bpuim

---
+++ bpium-test/precache-manifest.f0ffd88a4e4f321be9606529bf2b4ec7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "611d30a16d6178c4fa5f",
+    "url": "./static/js/main.ba0b983c.chunk.js"
+  },
+  {
+    "revision": "569f5bd38e0446ab8777",
+    "url": "./static/js/2.4488812f.chunk.js"
+  },
+  {
+    "revision": "611d30a16d6178c4fa5f",
+    "url": "./static/css/main.e245cc4e.chunk.css"
+  },
+  {
+    "revision": "6582d17eafbc0f0cf4f7e6a67618291a",
+    "url": "./index.html"
+  }
+];
