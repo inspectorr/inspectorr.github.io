3949cb28d53f14945ccc3e1ba50c77c4c6e5e35e
inspectorr <hmrfins@gmail.com>
2019-05-14 10:15:58 +0300

    up

---
+++ physics3n/precache-manifest.1164bff5aa72902e2aebbd694f72f8ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88e5d9f74a2c3fefdc8e0f3e163efb7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1bc88f8fb05d9760efd8",
+    "url": "./static/css/main.86102797.chunk.css"
+  },
+  {
+    "revision": "296f8c34ec4456141301",
+    "url": "./static/js/2.025f70b7.chunk.js"
+  },
+  {
+    "revision": "1bc88f8fb05d9760efd8",
+    "url": "./static/js/main.9ea489b3.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  }
+]);
