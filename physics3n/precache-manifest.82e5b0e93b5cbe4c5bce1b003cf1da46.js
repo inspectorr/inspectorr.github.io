bf7778b5c2f6f389f14c23e96f3d955b12a5537c
inspectorr <hmrfins@gmail.com>
2019-06-03 21:17:27 +0300

    up

--- physics3n/precache-manifest.82e5b0e93b5cbe4c5bce1b003cf1da46.js
+++ physics3n/precache-manifest.82e5b0e93b5cbe4c5bce1b003cf1da46.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "de399136c3735be2a7db1446993e6d6b",
+    "revision": "127e0e90f87583d8a42e98f850c17b52",
     "url": "./index.html"
   },
   {
@@ -8,19 +8,23 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.131549c7.chunk.css"
   },
   {
-    "revision": "a226fd2aaa6c6c970add",
-    "url": "./static/css/main.7a34a8a1.chunk.css"
+    "revision": "36ea69c0bfbd7b481212",
+    "url": "./static/css/main.dc10a5f3.chunk.css"
   },
   {
     "revision": "4566bfff739ea8034643",
     "url": "./static/js/2.138eda6f.chunk.js"
   },
   {
-    "revision": "a226fd2aaa6c6c970add",
-    "url": "./static/js/main.3dd90968.chunk.js"
+    "revision": "36ea69c0bfbd7b481212",
+    "url": "./static/js/main.c939b3ae.chunk.js"
   },
   {
     "revision": "9eb600ee07a27cdad64f",
     "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "2cf54361befb3521cf48ce7de9a12ee2",
+    "url": "./static/media/stars.2cf54361.jpg"
   }
 ]);
