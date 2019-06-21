48bb10f1c5c1780a0294678aafe0a4897fc312ad
inspectorr <hmrfins@gmail.com>
2019-06-21 15:34:31 +0300

    up

---
+++ fast-boss/precache-manifest.0f6449cea0407594a410c441115b0b4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd98f3ccdf7b4023055ad17b95edace3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33a783e860d04e2afafa",
+    "url": "/static/css/main.2c50feb0.chunk.css"
+  },
+  {
+    "revision": "cc344475849552567e56",
+    "url": "/static/js/2.d625b8d6.chunk.js"
+  },
+  {
+    "revision": "33a783e860d04e2afafa",
+    "url": "/static/js/main.bb4c8368.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "2868f795fcb5dc1c94b43ec40d9b1901",
+    "url": "/static/media/bg.2868f795.png"
+  },
+  {
+    "revision": "12fc53b1f8b9990985ac94b69e4c957d",
+    "url": "/static/media/logo.12fc53b1.png"
+  }
+]);
