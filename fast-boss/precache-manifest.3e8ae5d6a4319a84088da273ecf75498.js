16d98e579b980a177f7f8c7d1770cb78d66aa3a1
inspectorr <hmrfins@gmail.com>
2019-06-22 14:23:03 +0300

    up

---
+++ fast-boss/precache-manifest.3e8ae5d6a4319a84088da273ecf75498.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2ea001efd4aaa87f10604233bb0389",
+    "url": "/fast-boss/index.html"
+  },
+  {
+    "revision": "ed37f28914ad6cf638ff",
+    "url": "/fast-boss/static/css/main.5ff2fc70.chunk.css"
+  },
+  {
+    "revision": "ef1e281c265d0aa16266",
+    "url": "/fast-boss/static/js/2.6cf77a81.chunk.js"
+  },
+  {
+    "revision": "ed37f28914ad6cf638ff",
+    "url": "/fast-boss/static/js/main.9c97a4d4.chunk.js"
+  },
+  {
+    "revision": "82dd68e249569fa76e8e",
+    "url": "/fast-boss/static/js/runtime~main.4c4910f7.js"
+  },
+  {
+    "revision": "2868f795fcb5dc1c94b43ec40d9b1901",
+    "url": "/fast-boss/static/media/bg.2868f795.png"
+  },
+  {
+    "revision": "12fc53b1f8b9990985ac94b69e4c957d",
+    "url": "/fast-boss/static/media/logo.12fc53b1.png"
+  },
+  {
+    "revision": "fac0c2cb19b0e2e0138674d279878f6e",
+    "url": "/fast-boss/static/media/logo_sm.fac0c2cb.png"
+  }
+]);
