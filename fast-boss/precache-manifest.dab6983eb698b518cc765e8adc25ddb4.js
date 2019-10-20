de17e3906a90e904a28bf0d433211cba2a4468ba
Roman <hmrfins@gmail.com>
2019-10-20 16:01:24 +0300

    up

---
+++ fast-boss/precache-manifest.dab6983eb698b518cc765e8adc25ddb4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e746ffe79dfcf2eae284d7a81cc22a0b",
+    "url": "/fast-boss/index.html"
+  },
+  {
+    "revision": "6911780cf7606866c906",
+    "url": "/fast-boss/static/css/2.44a5c8d7.chunk.css"
+  },
+  {
+    "revision": "4effbd4d0adfc2d5d7f0",
+    "url": "/fast-boss/static/css/main.9a3de97a.chunk.css"
+  },
+  {
+    "revision": "6911780cf7606866c906",
+    "url": "/fast-boss/static/js/2.d7364527.chunk.js"
+  },
+  {
+    "revision": "4effbd4d0adfc2d5d7f0",
+    "url": "/fast-boss/static/js/main.5c1d5733.chunk.js"
+  },
+  {
+    "revision": "82dd68e249569fa76e8e",
+    "url": "/fast-boss/static/js/runtime~main.4c4910f7.js"
+  },
+  {
+    "revision": "3586b17e464b6847127ee9e79f85b19e",
+    "url": "/fast-boss/static/media/adam.3586b17e.jpg"
+  },
+  {
+    "revision": "2868f795fcb5dc1c94b43ec40d9b1901",
+    "url": "/fast-boss/static/media/bg.2868f795.png"
+  },
+  {
+    "revision": "12fc53b1f8b9990985ac94b69e4c957d",
+    "url": "/fast-boss/static/media/logo.12fc53b1.png"
+  },
+  {
+    "revision": "89d14e41b2e3292ccb4c6253b41aca79",
+    "url": "/fast-boss/static/media/logo.89d14e41.png"
+  },
+  {
+    "revision": "fac0c2cb19b0e2e0138674d279878f6e",
+    "url": "/fast-boss/static/media/logo_sm.fac0c2cb.png"
+  },
+  {
+    "revision": "e699e493f2b97ab86d8ad8c7e517fc40",
+    "url": "/fast-boss/static/media/robot.e699e493.jpg"
+  }
+]);
--- fast-boss/service-worker.js
+++ fast-boss/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7fb94ec0f0dedaea7e45c2ffa3bc5dfd.js"
+  "/fast-boss/precache-manifest.dab6983eb698b518cc765e8adc25ddb4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fast-boss/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
