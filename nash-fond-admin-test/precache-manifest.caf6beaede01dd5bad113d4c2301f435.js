b16a968fa3fe9ab68eb6f70badb01cbab8565648
Roman <hmrfins@gmail.com>
2019-12-11 01:27:53 +0300

    nf-admin-test

---
+++ nash-fond-admin-test/precache-manifest.caf6beaede01dd5bad113d4c2301f435.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa844f2bfdcc6e30820d3ebec4a50361",
+    "url": "/nash-fond-admin-test/index.html"
+  },
+  {
+    "revision": "7872f4e3dbb95ea2a93d",
+    "url": "/nash-fond-admin-test/static/css/main.41c711c9.chunk.css"
+  },
+  {
+    "revision": "6501515c72df781b5b6e",
+    "url": "/nash-fond-admin-test/static/js/2.77c51c9c.chunk.js"
+  },
+  {
+    "revision": "7872f4e3dbb95ea2a93d",
+    "url": "/nash-fond-admin-test/static/js/main.86b4b3db.chunk.js"
+  },
+  {
+    "revision": "be7f8e4d6a0b258e510b",
+    "url": "/nash-fond-admin-test/static/js/runtime-main.8cee7c2b.js"
+  }
+]);
